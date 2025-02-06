c5fd28a7f41eff9bec333b12ce91cbd8702d627f
m74jji <pytaek1@gmail.com>
2025-02-06 17:39:22 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb",
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "storage.googleapis.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
